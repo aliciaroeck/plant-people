bd72af024c9c2b41012f1bc1a5beb72b3d7a2cf1
aliciaroeck16 <aliciamata00@gmail.com>
2020-07-17 18:36:38 -0500

    heroku

--- plant_people/settings.py
+++ plant_people/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7qp%!joi8kifvdk_j39y&_9_ozx_3pl=gkofiq6@#*-6r)%wln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [https://plant-people.herokuapp.com/]
 
 # Application definition
 
